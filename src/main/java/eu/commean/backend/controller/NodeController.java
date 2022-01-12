4957dfce6871ba5b009ed747abe4ac0d4c4adf65
lumi200 <luca.nachbar@htl-klu.at>
2022-01-12 16:09:11 +0100

    Add missing endpoints

--- src/main/java/eu/commean/backend/controller/NodeController.java
+++ src/main/java/eu/commean/backend/controller/NodeController.java
@@ -1,21 +1,24 @@
 package eu.commean.backend.controller;
 
-import java.util.List;
-
+import eu.commean.backend.data.TrafficCameraNode;
+import eu.commean.backend.dto.MapOverlayGeoJson;
+import eu.commean.backend.dto.node.CreateNodeDto;
+import eu.commean.backend.dto.node.NodeDto;
+import eu.commean.backend.dto.node.NodeGeoJsonDto;
+import eu.commean.backend.service.CrossroadService;
+import eu.commean.backend.service.TrafficCameraNodeService;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import eu.commean.backend.dto.MapOverlayGeoJson;
-import eu.commean.backend.dto.NodeDto;
-import eu.commean.backend.service.CrossroadService;
+import java.util.List;
+import java.util.UUID;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/v1/nodes")
 public class NodeController {
@@ -23,14 +26,16 @@ public class NodeController {
 	@Autowired
 	private CrossroadService cs;
 	@Autowired
+	private TrafficCameraNodeService tcns;
+	@Autowired
 	private ModelMapper modelMapper;
 
-	@GetMapping(value = "/geojson")
+	@GetMapping(value = "/geojson", produces = "application/json")
 	@ResponseStatus(code = HttpStatus.OK)
 	public Object getAllNodes() {
 
-		List<NodeDto> data = cs.getAllCrossroads().stream().map(crossroad -> modelMapper.map(crossroad, NodeDto.class))
-				.toList();
+		List<NodeGeoJsonDto> data = tcns.getAllTrafficCameraNodes().stream()
+				.map(crossroad -> modelMapper.map(crossroad, NodeGeoJsonDto.class)).toList();
 		MapOverlayGeoJson mogj = new MapOverlayGeoJson(data);
 
 		if (data.isEmpty())
@@ -40,11 +45,30 @@ public class NodeController {
 
 	}
 
-	// TODO Make Endpoint: Get Node
-	@GetMapping(value = "")
+	@GetMapping(value = "", produces = "application/json")
 	@ResponseStatus(code = HttpStatus.OK)
-	public Object getNode(@RequestParam("id") String uuid) {
-		return null;
+	public Object getNode(@RequestParam("id") UUID uuid) {
+		return NodeDto.convertToDto(tcns.getTrafficCameraNodeById(uuid));
 	}
-	//TODO Make Endpoint: Register new Node
+
+	// TODO: Implement Registration-Key and generation of API-Key
+	@PostMapping(value = "", consumes = "application/json", produces = "text/text")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public String createNode(@RequestBody CreateNodeDto nodeToCreate) {
+		log.debug("CreateNodeDto| Id: {}, RegKey: {}", nodeToCreate.getId(), nodeToCreate.getRegistrationKey());
+		tcns.addTrafficCameraNode(new TrafficCameraNode(nodeToCreate.getId()));
+
+		log.debug("NodeOnDB: {}", tcns.getTrafficCameraNodeById(nodeToCreate.getId()).getId());
+		return "apiKeyGoesHere";
+
+	}
+
+	//Exeptions
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Error in Json (Body)")
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public void handleJsonParseExeption() {
+
+	}
+
+
 }
