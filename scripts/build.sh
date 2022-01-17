9b974f0859c77cc974b60a3e8a2ced89fe1480c8
2tefan <2tefan@tuta.io>
2022-01-17 19:07:51 +0100

    Add check for appilcation.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 . $(dirname "$0")/common.sh
 
+export APP_YML="$REPO_DIR/src/main/resources/application.yml"
+
+if [ ! -e "$APP_YML" ]
+then
+    echo "$(realpath $APP_YML) is missing!"
+    exit 1
+fi
+
 mvn --file $REPO_DIR package
