cc119c5107be571dd63a5b64b5260eef17afab6f
2tefan <2tefan@tuta.io>
2022-01-17 17:44:27 +0100

    Check for application.yml

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,9 @@
 . $PSScriptRoot\common.ps1
+
+$pathToAppYML = "$repoRoot\src\main\resources\application.yml"
+
+if(!(Test-Path $pathToAppYML -PathType Leaf)) {
+    throw "$pathToAppYML is missing!"
+}
+
 mvn --file $repoRoot package
