057d576373e97903c625cc61c006dec3368c299c
lumi200 <luca.nachbar@htl-klu.at>
2022-02-04 15:56:10 +0100

    Save api-key for test

--- src/test/java/eu/commean/backend/NodeEndpointTests.java
+++ src/test/java/eu/commean/backend/NodeEndpointTests.java
@@ -1,12 +1,10 @@
 package eu.commean.backend;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import eu.commean.backend.dto.node.CreateNodeDto;
 import eu.commean.backend.dto.node.NodeDto;
 import lombok.extern.log4j.Log4j2;
-import org.apiguardian.api.API;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +17,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,8 +31,6 @@ public class NodeEndpointTests {
 
 
 	private final UUID nodeUUID = UUID.fromString("4e8f0fd7-d936-42f9-9fd1-7b537f3ba690");
-	private static String API_KEY;
-
 	ObjectMapper mapper = new ObjectMapper();
 	@Autowired
 	MockMvc mockMvc;
@@ -50,21 +44,21 @@ public class NodeEndpointTests {
 						.content(mapper.writeValueAsString(createNodeDto)))
 				.andExpect(status().isCreated())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.api-key", Matchers.matchesRegex("^(?:[A-Za-z\\d+/]{4})*(?:[A-Za-z\\d+/]{3}=|[A-Za-z\\d+/]{2}==)?$"))).andReturn();
-		API_KEY = JsonPath.parse(result.getResponse().getContentAsString()).read("$.api-key").toString();
-		log.debug(API_KEY);
+		System.setProperty("commean-junit-apikey", JsonPath.parse(result.getResponse().getContentAsString()).read("$.api-key").toString());
+		log.debug(System.getProperty("commean-junit-apikey"));
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName(value = "Update Node")
 	public void updateNode() throws Exception {
-		double[] position = {1.0,1.0};
-		NodeDto nodeDto = new NodeDto(nodeUUID,"TEST001",position);
+		double[] position = {1.0, 1.0};
+		NodeDto nodeDto = new NodeDto(nodeUUID, "TEST001", position);
 
-		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/nodes").header("api-key", API_KEY)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(mapper.writeValueAsString(nodeDto)))
- 				.andExpect(status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/nodes").header("api-key", System.getProperty("commean-junit-apikey"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(mapper.writeValueAsString(nodeDto)))
+				.andExpect(status().isOk());
 
 
 	}
@@ -73,7 +67,7 @@ public class NodeEndpointTests {
 	@Order(3)
 	@DisplayName(value = "Get Node")
 	public void getNode() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/nodes").contentType(MediaType.APPLICATION_JSON).queryParam("id",nodeUUID.toString()))
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/nodes").contentType(MediaType.APPLICATION_JSON).queryParam("id", nodeUUID.toString()))
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(nodeUUID.toString()));
 	}
