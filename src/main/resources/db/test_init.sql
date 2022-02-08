10847095c5ca6e2a2b6656ddd0bcc0fe3d890ec8
lumi200 <luca.nachbar@htl-klu.at>
2022-02-08 10:45:05 +0100

    Update test_init.sql, MeasurementEndpointTest.java, and NodeEndpointTests.java

---
+++ src/main/resources/db/test_init.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO public.nodes (id, "location", "name") VALUES('8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid, 'POINT(14.30384 46.61683)', 'TEST_00');
+
+INSERT INTO public.apikey (id, "key") VALUES('8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid, 'tHt3YvjBtuHYYxzRyu1prjdX7iTioTwn+/nmMiHzGYs=');
+
+
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('36632b8d-b286-4ea8-b062-ee9b474cca63'::uuid, 5, 0, '2022-02-08 08:28:28.000', 0, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('3d7bb4f8-8dab-446b-88ca-040ba77e156e'::uuid, 5, 1, '2022-02-08 08:28:43.000', 0, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('6760df2a-ef8c-44f9-aa4f-134d72e49f88'::uuid, 5, 0, '2022-02-08 08:28:58.000', 0, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('276689c3-ccfa-4961-8a8a-5758ae7baa7a'::uuid, 5, 1, '2022-02-08 08:29:13.000', 0, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('7e480905-ead4-4807-92df-3783d5631968'::uuid, 5, 0, '2022-02-08 08:29:28.000', 1, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('23270489-cd1b-4e45-82c1-89be29a08b1e'::uuid, 5, 0, '2022-02-08 08:29:43.000', 2, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('a3701f43-78fe-4e29-9156-8e84830af0fe'::uuid, 5, 0, '2022-02-08 08:29:58.000', 1, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('dbc48ca8-247a-4540-a682-1765031e69dc'::uuid, 5, 0, '2022-02-08 08:30:13.000', 2, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('2d733f14-b93d-4d77-97fc-032224e251b7'::uuid, 5, 0, '2022-02-08 08:30:28.000', 1, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('8c2601e0-8ac4-44e6-b85e-e889a0238a14'::uuid, 5, 1, '2022-02-08 08:30:43.000', 2, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+INSERT INTO public.traffic_measurement (id, average_time_in_picture, cars, "timestamp", trucks, trafficcameranode_id) VALUES('6d3a2246-8c6a-4200-9139-19d879ba0f9d'::uuid, 5, 0, '2022-02-08 08:30:58.000', 3, '8db875b4-fe87-48fb-aeee-06c6c7bd378f'::uuid);
+
--- src/test/java/eu/commean/backend/NodeEndpointTests.java
+++ src/test/java/eu/commean/backend/NodeEndpointTests.java
@@ -7,11 +7,13 @@ import eu.commean.backend.dto.node.NodeDto;
 import lombok.extern.log4j.Log4j2;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,7 +32,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class NodeEndpointTests {
 
 
-	private final UUID nodeUUID = UUID.fromString("4e8f0fd7-d936-42f9-9fd1-7b537f3ba690");
+	private final UUID NODE_UUID = UUID.fromString("4e8f0fd7-d936-42f9-9fd1-7b537f3ba690");
+	private String API_KEY = "";
 	ObjectMapper mapper = new ObjectMapper();
 	@Autowired
 	MockMvc mockMvc;
@@ -39,13 +42,14 @@ public class NodeEndpointTests {
 	@Order(1)
 	@DisplayName(value = "Create Node")
 	public void createNode() throws Exception {
-		CreateNodeDto createNodeDto = new CreateNodeDto(nodeUUID, "NYI");
+		CreateNodeDto createNodeDto = new CreateNodeDto(NODE_UUID, "NYI");
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/nodes").contentType(MediaType.APPLICATION_JSON)
 						.content(mapper.writeValueAsString(createNodeDto)))
 				.andExpect(status().isCreated())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.api-key", Matchers.matchesRegex("^(?:[A-Za-z\\d+/]{4})*(?:[A-Za-z\\d+/]{3}=|[A-Za-z\\d+/]{2}==)?$"))).andReturn();
-		System.setProperty("commean-junit-apikey", JsonPath.parse(result.getResponse().getContentAsString()).read("$.api-key").toString());
-		log.debug(System.getProperty("commean-junit-apikey"));
+		log.debug(JsonPath.parse(result.getResponse().getContentAsString()).read("$.api-key").toString());
+		System.setProperty("commean-apikey",JsonPath.parse(result.getResponse().getContentAsString()).read("$.api-key").toString());
+
 	}
 
 	@Test
@@ -53,9 +57,9 @@ public class NodeEndpointTests {
 	@DisplayName(value = "Update Node")
 	public void updateNode() throws Exception {
 		double[] position = {1.0, 1.0};
-		NodeDto nodeDto = new NodeDto(nodeUUID, "TEST001", position);
+		NodeDto nodeDto = new NodeDto(NODE_UUID, "TEST001", position);
 
-		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/nodes").header("api-key", System.getProperty("commean-junit-apikey"))
+		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/nodes").header("api-key", System.getProperty("commean-apikey"))
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(mapper.writeValueAsString(nodeDto)))
 				.andExpect(status().isOk());
@@ -67,9 +71,13 @@ public class NodeEndpointTests {
 	@Order(3)
 	@DisplayName(value = "Get Node")
 	public void getNode() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/nodes").contentType(MediaType.APPLICATION_JSON).queryParam("id", nodeUUID.toString()))
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/nodes").contentType(MediaType.APPLICATION_JSON).queryParam("id", NODE_UUID.toString()))
 				.andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(nodeUUID.toString()));
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(NODE_UUID.toString()));
 	}
-
+	@AfterAll
+	public static void finishNodeTest()
+	{
+		System.clearProperty("commean-apikey");
+	}
 }
