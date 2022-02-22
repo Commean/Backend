f9f10ebd6cd6de1c91d985cc2bb7312006bc9201
lumi200 <luca.nachbar@htl-klu.at>
2022-02-22 09:00:00 +0100

    Add findByName

--- src/main/java/eu/commean/backend/repo/RoleRepository.java
+++ src/main/java/eu/commean/backend/repo/RoleRepository.java
@@ -1,9 +1,12 @@
 package eu.commean.backend.repo;
 
 import eu.commean.backend.entity.Role;
+import eu.commean.backend.enums.RoleName;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public interface RoleRepository extends CrudRepository<Role, UUID> {
+	Role findByName(RoleName name);
+
 }
